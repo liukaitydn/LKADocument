224853f316c70bb670df167b57f404c9c4f21c4d
liuk168@163.com <liuk168@163.com>
2020-06-28 11:24:50 +0800

    增加maven中央仓库配置

--- LKADocument/src/main/java/com/lk/api/LKADemoApplication.java
+++ LKADocument/src/main/java/com/lk/api/LKADemoApplication.java
@@ -7,7 +7,7 @@ import com.lk.api.annotation.LKADocument;
 
 @SpringBootApplication
 @LKADocument(basePackages="com.lk.api",projectName="LKADocument测试项目",description="智能、便捷、高效",
-serverNames="192.168.0.77:8080",version="1.0",enabled=true)
+serverNames="192.168.0.77:9010,192.168.0.77:8888",version="1.0",enabled=true)
 public class LKADemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LKADemoApplication.class, args);
